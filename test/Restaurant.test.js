26011d5feddba2444be30f223af075b9188ec584
Michael Bailey <michaelbailey1@fb.com>
2020-10-01 15:11:37 +0100

    added restaurant tests

---
+++ test/Restaurant.test.js
@@ -0,0 +1,34 @@
+let {Restaurant} = require("../src/classes/Restaurant")
+let {db} = require("../src/database")
+
+describe("restaurant tests", () => {
+    beforeAll((done) => {
+        db.run('CREATE TABLE IF NOT EXISTS restaurants(id INTEGER PRIMARY KEY, name TEXT, image TEXT);', done)
+    })
+
+    test("test restaurant creation", async (done) => {
+        let restaurant = await new Restaurant({name: "The three pilchards", image: "https://www.example.com/"})
+
+        expect(restaurant.name).toBe("The three pilchards")
+        expect(restaurant.image).toBe("https://www.example.com/")
+        db.all("SELECT * FROM restaurants WHERE id = 1", (err, rows) => {
+            console.log(restaurant);
+            expect(rows[0].id).toBe(restaurant.id)
+            done()
+        })
+
+    })
+})
+
+test('can create an instance of an restaurant from a row', (done) => {
+    db.get('SELECT * FROM restaurants WHERE id=?', 1, async (err, row) => {
+        const restaurant = await new Restaurant(row)
+        expect(restaurant.id).toBe(1)
+        expect(restaurant.name).toBe('The three pilchards')
+        db.get('SELECT COUNT(id) AS total FROM restaurants;', (err, count) => {
+            expect(count.total).toBe(1)
+            console.log(count)
+            done()
+        })
+    })        
+})
