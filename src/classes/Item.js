5581dad3ea9657d343040626fc0c976a5e0ff149
Michael Bailey <michaelbailey1@fb.com>
2020-10-01 16:15:33 +0100

    added menuID

--- src/classes/Item.js
+++ src/classes/Item.js
@@ -2,7 +2,8 @@ const { db } = require("../database")
 
 class Item {
     name = ""
-    price
+    price = 0.00
+    menuID = 0
 
     static default = new Item({name: "std dish", price: 10.00})
 
@@ -24,6 +25,7 @@ class Item {
         this.id
         this.name = data.name
         this.price = data.price
+        this.menuID = data.menuID
 
         if (this.id) {
 
@@ -35,11 +37,11 @@ class Item {
 
             return new Promise((res, rej) => {
                 // attmpt to create a table
-                db.all("CREATE TABLE IF NOT EXISTS items(id INTEGER PRIMARY KEY, name TEXT, price FLOAT)", function(err) {
+                db.all("CREATE TABLE IF NOT EXISTS items(id INTEGER PRIMARY KEY, name TEXT, price FLOAT, menuID INTEGER)", function(err) {
                     if (err) console.log(err)
 
                     // attempt to insert the new object into the database
-                    db.run("INSERT INTO items(name, price) VALUES(?,?)", [newItem.name, newItem.price], function(err) {
+                    db.run("INSERT INTO items(name, price, menuID) VALUES(?,?,?)", [this.name, this.price, this.menuID], function(err) {
                         if (err) console.log(err);
 
                         newItem.id = this.lastID
