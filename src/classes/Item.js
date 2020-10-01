52af436c53b5eee522f84c6649e63a7ffa367069
Michael Bailey <michaelbailey1@fb.com>
2020-10-01 15:39:31 +0100

    converted item to async and orm

--- src/classes/Item.js
+++ src/classes/Item.js
@@ -1,23 +1,43 @@
+const { db } = require("../database")
 
 class Item {
     name = ""
-    ingredients = []
     price
 
-    static default = new Item("std dish", [], 10.00)
+    static default = new Item({name: "std dish", price: 10.00})
 
-    constructor(name, ingredients, price) {
-        if (!name) throw new Error("no name provided")
-        if (!ingredients) throw new Error("no ingredients provided")
-        if (!price) throw new Error("no price provided")
+    constructor(data) {
 
-        this.name = name
-        this.ingredients = ingredients
-        this.price = price
-    }
+        if (!data.name) throw new Error("no name provided")
+        if (!data.price) throw new Error("no price provided")
 
-    getAllergens() {
-        return this.ingredients.filter(i => i.isAllergen)
+        this.id
+        this.name = data.name
+        this.price = data.price
+
+        if (this.id) {
+
+            // if id exists then retrun a resolved promise
+            return Promise.resolve(this)
+        } else {
+            // reassign this to a variable
+            let newItem = this
+
+            return new Promise((res, rej) => {
+                // attmpt to create a table
+                db.all("CREATE TABLE IF NOT EXISTS items(id INTEGER PRIMARY KEY, name TEXT, price FLOAT)", function(err) {
+                    if (err) console.log(err)
+
+                    // attempt to insert the new object into the database
+                    db.run("INSERT INTO items(name, price) VALUES(?,?)", [newItem.name, newItem.price], function(err) {
+                        if (err) console.log(err);
+
+                        newItem.id = this.lastID
+                        res(newItem)
+                    })
+                })
+            })
+        }
     }
 }
 
--- test/Item.test.js
+++ test/Item.test.js
@@ -2,29 +2,15 @@ const { Item } = require("../src/classes/Item")
 const { Ingredient } = require("../src/classes/Ingredient") 
 
 describe("Item tests", () => {
-    test("test Item creation", () => {
-        let pasta = new Ingredient("pasta", true)
-        let meat = new Ingredient("mince beef", false)
-        let Tomatoes = new Ingredient("Tomato", false)
+    test("test Item creation", async (done) => {
         
-        let item = new Item("Spaghetti Bolognese", [pasta, meat, Tomatoes], 12.50)
+        let item = await new Item({name: "Spaghetti Bolognese", price: 12.50})
 
         expect(item instanceof Item).toBeTruthy()
         expect(item.name).toEqual("Spaghetti Bolognese")
-        expect(item.ingredients.length).toEqual(3)
         
-        expect(() => {new Item()}).toThrow()
-        expect(() => {new Item("Spaghetti Bolognese")}).toThrow()
-        expect(() => {new Item("Spaghetti Bolognese", [pasta, meat, Tomatoes])}).toThrow()
-    })
-
-    test("test get allergens", () => {
-        let pasta = new Ingredient("pasta", true)
-        let meat = new Ingredient("mince beef", false)
-        let Tomatoes = new Ingredient("Tomato", false)
-        
-        let item = new Item("Spaghetti Bolognese", [pasta, meat, Tomatoes], 12.50)
-
-        expect(item.getAllergens()).toEqual([pasta])
+        expect(() => {new Item({})}).toThrow()
+        expect(() => {new Item({name: "Spaghetti Bolognese"})}).toThrow()
+        done()
     })
 })
